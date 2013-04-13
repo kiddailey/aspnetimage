7c22d72090b2251b06e4846e9643ae959cf7f784
lucio.fassio <lucio.fassio@f487af35-832c-c109-6b6f-b5932b9d585f>
2013-04-13 13:56:34 +0000

    Added ResizeOnWidth and ResizeOnHeight to resize proportionally an image, these methods was not in the original ASPImage COM version

--- branches/dev/ASPNetImage/Resource/register.bat
+++ branches/dev/ASPNetImage/Resource/register.bat
@@ -1,3 +1,3 @@
 @echo off
-c:\Windows\Microsoft.NET\Framework\v4.0.30319\regasm.exe ASPImage.dll /tlb /codebase
+c:\Windows\Microsoft.NET\Framework\v4.0.30319\regasm.exe ASPNetImage.dll /tlb /codebase
 pause
--- branches/dev/testWeb/Default.aspx.designer.cs
+++ branches/dev/testWeb/Default.aspx.designer.cs
@@ -13,6 +13,15 @@ namespace testWeb {
     public partial class _Default {
         
         /// <summary>
+        /// Image1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Image Image1;
+        
+        /// <summary>
         /// Button1 control.
         /// </summary>
         /// <remarks>
